c05def199f583b29498c9cb641740beb8235515d
Ryan Broadsword <rbroadsword@gmail.com>
2022-08-12 16:51:47 -0700

    add models for Engineer/machine/EngineerMachine

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic; 
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>(); 
+    }
+
+    public int EngineerId { get; set; }
+    public string EngineerName { get; set; }
+    public string Specialty { get; set; }
+
+    public virtual ICollection<EngineerMachine> JoinEntities { get; }
+  }
+}
